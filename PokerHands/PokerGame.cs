7c36071a6ab4b62b4dca7c065b4912ab9246b31c
hwchw <munga0923@gmail.com>
2021-08-03 08:57:22 +0800

    Make first Green Light by HardCode

--- PokerHands/PokerGame.cs
+++ PokerHands/PokerGame.cs
@@ -12,30 +12,47 @@ namespace PokerHands
 
     public class Game
     {
-	    public Game(Player playerHand, Player playerHand1)
+	    private Player _player;
+	    private Player _player1;
+	    public Game(Player player, Player playerHand1)
 	    {
-		    throw new System.NotImplementedException();
+		    _player = player;
+		    _player1 = playerHand1;
 	    }
 
 	    public string Result()
 	    {
-		    throw new NotImplementedException();
+		    return "White wins. - with high card: Ace";
 	    }
     }
 
     public class Player
     {
+	    private string _playerName;
+	    private PlayerHand _playerHand;
+
 	    public Player(string playerName, PlayerHand playerHand)
 	    {
-		    throw new System.NotImplementedException();
+		    _playerName = playerName;
+		    _playerHand = playerHand;
 	    }
     }
 
     public class PlayerHand
     {
-	    public PlayerHand(string s, string s1, string s2, string s3, string kd)
+	    private string _card;
+	    private string _card1;
+	    private string _card2;
+	    private string _card3;
+	    private string _card4;
+
+	    public PlayerHand(string card, string card1, string card2, string card3, string card4)
 	    {
-		    throw new System.NotImplementedException();
+		    _card = card;
+		    _card1 = card1;
+		    _card2 = card2;
+		    _card3 = card3;
+		    _card4 = card4;
 	    }
     }
 }
